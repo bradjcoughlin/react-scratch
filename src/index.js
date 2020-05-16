32401606cefc364cbb5ceb474cf2f13f882036af
Brad Coughlin <brad@bradcoughlin.com>
2020-05-15 22:14:33 -0700

    working with babelrc file

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+
+const wrapper = document.getElementById("root");
+wrapper ? ReactDOM.render(<App />, wrapper) : false;
