64109bcb635560c534e3558d796c84968f03651d
Brad Coughlin <brad@bradcoughlin.com>
2020-05-15 23:27:41 -0700

    wip

--- src/Button.js
+++ src/Button.js
@@ -5,19 +5,30 @@ export default class Button extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            fade: false
+            fade: false,
+            poop: false,
         }
     }
     render() {
         const fade = this.state.fade
+        const poop = this.state.poop
             return (
-                <button
-                    ref='button'
-                    onClick={() => this.setState({ fade: true })}
-                    onAnimationEnd={() => this.setState({ fade: false })}
-                    className={fade ? 'fade' : ''}>
-                    Button
-                </button>
+                <div>
+                    <button
+                        ref='button'
+                        onClick={() => this.setState({ fade: true })}
+                        onAnimationEnd={() => this.setState({ fade: false })}
+                        className={fade ? 'fade' : ''}>
+                        Button
+                    </button>
+                    <button
+                        ref='poopButton'
+                        onClick={() => this.setState({ poop: true })}
+                        onAnimationEnd={() => this.setState({ poop: true })}
+                        className={poop ? 'poop' : ''}>
+                        Button
+                    </button>
+                </div>
             )
         }
 }
